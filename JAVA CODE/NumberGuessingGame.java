a3b99902f2ac475ca203087d7d0656e26c3f478b
SOUNDARIYA <soundariyasselvam@gmail.com>
2026-05-30 15:54:57 +0530

    Refactor NumberGuessingGame for cleaner output

--- JAVA CODE/NumberGuessingGame.java
+++ JAVA CODE/NumberGuessingGame.java
@@ -9,27 +9,20 @@ public class NumberGuessingGame {
         int number = rand.nextInt(100) + 1;
         int guess = 0;
         int attempts = 0;
-
-        System.out.println("================================");
         System.out.println("   Welcome to Number Guessing Game!");
         System.out.println("   Guess a number between 1 and 100");
-        System.out.println("================================");
-
         while (guess != number) {
             System.out.print("\nEnter your guess: ");
             guess = sc.nextInt();
             attempts++;
-
             if (guess > number) {
                 System.out.println("Too High! Try a lower number.");
             } else if (guess < number) {
                 System.out.println("Too Low! Try a higher number.");
             } else {
-                System.out.println("================================");
                 System.out.println("Correct! You guessed it!");
                 System.out.println("The number was : " + number);
                 System.out.println("Total attempts : " + attempts);
-                System.out.println("================================");
             }
         }
 
